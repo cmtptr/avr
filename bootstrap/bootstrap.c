4996fee5caa4b691aef8f4ba4a0f49249066b947
Corey Thompson <cmtptr@gmail.com>
2013-06-05 18:05:13 -0400

    bootstrap/bootstrap.c: Try blinking an LED using T0

--- bootstrap/bootstrap.c
+++ bootstrap/bootstrap.c
@@ -1,6 +1,18 @@
 #include <mcs51/p89v51rd2.h>
-int main()
+
+void blinker(void) __interrupt (TF0_VECTOR)
 {
-	SPSR = 0x12;
-	return 0;
+	P0_0 ^= 1;
+}
+
+void main()
+{
+	P0 = 0xff;
+	TMOD = T0_M1;
+	TH0 = 0;
+	ET0 = 1;
+	EA = 0;
+	TF0 = 1;
+	TR0 = 1;
+	while (1);
 }
