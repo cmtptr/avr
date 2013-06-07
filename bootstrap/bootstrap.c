a306aa7d1e98038eb4bf0dd055eee95515c422b0
Corey Thompson <cmtptr@gmail.com>
2013-06-06 20:49:22 -0400

    bootstrap/bootstrap.c: Succeed at blinking an LED using T0!

--- bootstrap/bootstrap.c
+++ bootstrap/bootstrap.c
@@ -2,17 +2,22 @@
 
 void blinker(void) __interrupt (TF0_VECTOR)
 {
+	static unsigned short i = 0;
+	if (i < 5000) {
+		++i;
+		return;
+	}
+	i = 0;
 	P0_0 ^= 1;
 }
 
 void main()
 {
-	P0 = 0xff;
-	TMOD = T0_M1;
-	TH0 = 0;
-	ET0 = 1;
-	EA = 0;
-	TF0 = 1;
-	TR0 = 1;
+	TMOD = T0_M1;  /* 8-bit auto-reload mode */
+	TH0 = -100;  /* call blinker() every 100 cycles */
+	ET0 = 1;  /* enable timer 0 interrupt */
+	EA = 1;  /* enable interrupts */
+	TF0 = 1;  /* trigger the interrupt to init TL0 */
+	TR0 = 1;  /* start timer 0 */
 	while (1);
 }
