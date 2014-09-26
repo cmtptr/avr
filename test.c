7e2a092f085ba7aff37baf49c87d51d82438a30a
Corey Thompson <cmtptr@gmail.com>
2014-09-25 22:12:06 -0400

    test.c: Configure PORTB for output and use PINB to toggle

--- test.c
+++ test.c
@@ -3,10 +3,11 @@
 
 int main(void)
 {
+	DDRB = 1 << DDB3 | 1 << DDB4;
 	PORTB = ~(1 << PB4);
 	while (1) {
 		_delay_ms(500);
-		PORTB ^= 1 << PB3 | 1 << PB4;
+		PINB |= 1 << PB3 | 1 << PB4;
 	}
 	return 0;
 }
